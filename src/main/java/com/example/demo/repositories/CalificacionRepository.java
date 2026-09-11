e36e6db266f99504fd1b76959f6ae30937b90c50
kronen9999 <stacica22eneronkio@gmail.com>
2026-09-10 22:10:58 -0600

    Calificaciones v1

--- src/main/java/com/example/demo/repositories/CalificacionRepository.java
+++ src/main/java/com/example/demo/repositories/CalificacionRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface CalificacionRepository extends JpaRepository<Calificacion, Long> {
 
     boolean existsByInscripcionId(Long idInscripcion);
+
+    boolean existsByInscripcionIdAndIdNot(Long idInscripcion, Long id);
 }
